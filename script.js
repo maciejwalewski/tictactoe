f886469c062b3bc4ef655311947279ccda814ced
maciejwalewski <maciekwalewski2@gmail.com>
2018-01-21 19:55:40 +0100

    red & blue player + first row working
    red & blue player + first row working

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-var square = document.querySelectorAll('.square');  //array with all squares(0-8)
-console.log(square); 
+    var square = document.querySelectorAll('.field div');  //array with all squares(0-8)
+    console.log(square); 
 
-for(i=0; i<square.length; i++){
-    square[i].addEventListener('click', function(){
-        this.setAttribute('style', 'background-color: red;');
-    })
-};
+    var currentPlayer;
+    var empty;
+
+    startGame();
 
 
+    function startGame(){
+        empty = 9;
+        for(i=0; i<square.length; i++){
+            currentPlayer = 'red';
+            square[i].addEventListener('click', function(){
+                this.classList.add(currentPlayer);
+                empty--;
+                if(currentPlayer === 'red'){
+                    currentPlayer = 'blue';
+                } else {
+                    currentPlayer = 'red';
+                }
+
+                setTimeout(function(){
+                    if(empty === 0){
+                        winner();
+                    }
+                }, 200);
+            });
+        };
+    };
+
+    function winner(){
+        var row1 = square[0].className + square[1].className + square[2].className;
+        console.log(row1);
+        if(row1 === 'redredred'){
+            alert('red wins!')
+        };
+    };
 
 });
